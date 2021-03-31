9ad509dfb4a9c67c555257fd838efb796977bd21
alexsedman <77463724+alexsedman@users.noreply.github.com>
2021-03-31 17:46:21 +0100

    final commit

--- calculator.py
+++ calculator.py
@@ -1,5 +1,5 @@
 import os
-portfolio = float(0)
+portfolio = 0
 invested = 0
 counter = 0
 
@@ -9,18 +9,19 @@ month = int(input("How many months passed? "))
 while counter != month:
     counter += 1
     if counter == 1:
-        portfolio = round(portfolio + 1000.0, 2)
+        portfolio += 1000
         invested += 1000
     else:
-        portfolio = round(portfolio + 100.0, 2)
+        portfolio += 100
         invested += 100
-    portfolio = round(portfolio * 1.02, 2)
+    portfolio *= 1.02
+    portfolio = int(portfolio)
     print("Month " + str(counter) + " portfolio value: " + str(portfolio))
     print("Month " + str(counter) + " amount invested: " + str(invested))
 
 print("Portfolio Value: " + str(portfolio))
 print("Amount Invested: " + str(invested))
-profit = round(portfolio - invested, 2)
+profit = portfolio - invested
 print("Profit: " + str(profit))
 input("Press enter to continue")
 
